be747db020019526d274666256af82fdb1a09901
daimou1028 <daimou1028@gmail.com>
2019-02-23 00:31:09 +0900

    test

---
+++ welcome.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * Fuel is a fast, lightweight, community driven PHP 5.4+ framework.
+ *
+ * @package    Fuel
+ * @version    1.8.1
+ * @author     Fuel Development Team
+ * @license    MIT License
+ * @copyright  2010 - 2018 Fuel Development Team
+ * @link       http://fuelphp.com
+ */
+
+/**
+ * The Welcome Controller.
+ *
+ * A basic controller example.  Has examples of how to set the
+ * response body and status.
+ *
+ * @package  app
+ * @extends  Controller
+ */
+class Controller_Welcome extends Controller
+{
+	/**
+	 * The basic welcome message
+	 *
+	 * @access  public
+	 * @return  Response
+	 */
+	public function action_index()
+	{
+		$coupon_code = 'ABCDEF';
+
+		$query = "
+			SELECT
+					coupon.coupon_id,
+					coupon.name,
+					count(DISTINCT coupon_used.coupon_used_id) as coupon_used_count,
+					count(DISTINCT coupon_relation_product.id) as coupon_product_count
+			FROM
+					coupon_code
+
+				INNER JOIN
+					coupon
+				ON
+					coupon.coupon_id = coupon_code.coupon_id
+
+				LEFT JOIN
+					coupon_used
+				ON
+					coupon_used.coupon_id = coupon.coupon_id
+
+				LEFT JOIN
+					coupon_relation_product
+				ON
+					coupon_relation_product.coupon_id = coupon.coupon_id
+
+			WHERE
+					coupon_code.coupon_code = :coupon_code
+			GROUP BY
+					coupon.coupon_id
+		";
+		$result = DB::query($query)->bind('coupon_code', $coupon_code)->execute();
+
+		// print_r($result->as_array());
+
+		header("Content-Type: application/json; charset=utf-8");
+		return json_encode($result->as_array());
+
+		// return Response::forge(View::forge('welcome/index'));
+	}
+
+	/**
+	 * A typical "Hello, Bob!" type example.  This uses a Presenter to
+	 * show how to use them.
+	 *
+	 * @access  public
+	 * @return  Response
+	 */
+	public function action_hello()
+	{
+		return Response::forge(Presenter::forge('welcome/hello'));
+	}
+
+	/**
+	 * The 404 action for the application.
+	 *
+	 * @access  public
+	 * @return  Response
+	 */
+	public function action_404()
+	{
+		return Response::forge(Presenter::forge('welcome/404'), 404);
+	}
+}
