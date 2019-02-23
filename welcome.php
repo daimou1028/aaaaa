ce32459ec728d8f52456c393ef8f4d0b503fb0fa
daimou1028 <daimou1028@gmail.com>
2019-02-24 04:39:30 +0900

    test

--- welcome.php
+++ welcome.php
@@ -30,20 +30,25 @@ class Controller_Welcome extends Controller
 	public function action_index()
 	{
 		$coupon_code = 'ABCDEF';
+		$member_id = 1;
 
 		$query = "
 			SELECT
 					coupon.coupon_id,
-					coupon.name,
+					coupon.coupon_name,
 					count(DISTINCT coupon_used.coupon_used_id) as coupon_used_count,
-					count(DISTINCT coupon_relation_product.id) as coupon_product_count
+					count(DISTINCT coupon_relation_product.coupon_relation_product_id) as coupon_product_count
 			FROM
 					coupon_code
+
+
 
 				INNER JOIN
 					coupon
 				ON
 					coupon.coupon_id = coupon_code.coupon_id
+
+
 
 				LEFT JOIN
 					coupon_used
@@ -55,12 +60,25 @@ class Controller_Welcome extends Controller
 				ON
 					coupon_relation_product.coupon_id = coupon.coupon_id
 
+				LEFT JOIN
+					cart
+				ON
+					cart.product_id = coupon_relation_product.product_id
+				AND
+					cart.member_id = :member_id
+
+
+
 			WHERE
 					coupon_code.coupon_code = :coupon_code
+			AND
+					coupon_code.coupon_code_used = 0
+
+					
 			GROUP BY
 					coupon.coupon_id
 		";
-		$result = DB::query($query)->bind('coupon_code', $coupon_code)->execute();
+		$result = DB::query($query)->bind('coupon_code', $coupon_code)->bind('member_id', $member_id)->execute();
 
 		// print_r($result->as_array());
 
